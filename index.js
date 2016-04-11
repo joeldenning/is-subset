1d0d1396acad961fa6b93c8bf79ca921a766d597
Joel Denning <joeldenning@gmail.com>
2016-04-11 16:55:24 -0600

    Adding built files to version control.

---
+++ index.js
@@ -0,0 +1,40 @@
+/**
+ * Check if an object is contained within another object.
+ *
+ * Returns `true` if:
+ * - all enumerable keys of *subset* are also enumerable in *superset*, and
+ * - every value assigned to an enumerable key of *subset* strictly equals
+ *   the value assigned to the same key of *superset* – or is a subset of it.
+ *
+ * @param  {Object}  superset
+ * @param  {Object}  subset
+ *
+ * @returns  {Boolean}
+ *
+ * @module    is-subset
+ * @function  default
+ * @alias     isSubset
+ */
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true
+});
+var isSubset = function isSubset(superset, subset) {
+  if (typeof superset !== 'object' || superset === null || typeof subset !== 'object' || subset === null) return false;
+
+  if (superset instanceof Date || subset instanceof Date) return superset.valueOf() === subset.valueOf();
+
+  return Object.keys(subset).every(function (key) {
+    if (!superset.propertyIsEnumerable(key)) return false;
+
+    var subsetItem = subset[key];
+    var supersetItem = superset[key];
+    if (typeof subsetItem === 'object' && subsetItem !== null ? !isSubset(supersetItem, subsetItem) : supersetItem !== subsetItem) return false;
+
+    return true;
+  });
+};
+
+exports['default'] = isSubset;
+module.exports = exports['default'];
